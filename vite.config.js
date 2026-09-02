dbb505b74cfe1fdc73438856c88c9a36c1d8b129
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-02 16:56:19 +0200

    svelte setup fix vite.config #2

--- vite.config.js
+++ vite.config.js
@@ -16,10 +16,6 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
-		}),
-		velte({
-		configFile: false
-		// your Svelte config here
 		})
 	]
 });
