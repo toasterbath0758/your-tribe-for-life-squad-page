3513868f0826d27a0fbbbea04525ee8444f7fe53
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-02 15:02:44 +0200

    Inline stylesheet setup #3

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -12,3 +12,36 @@ Const persons = data.persons;
 
  <h1> <h1>
 <p> hallo mensen </p>
+ 
+<style>
+
+/* font-sizes */
+:root {
+    --font-size-xxl: 2.8em;
+    --font-size-xl: 2em;
+    --font-size-l: 1.8em;
+    --font-size-m: 1em;
+    --font-size-s: 0.8em;
+    --font-size-xs: 0.5em;
+    --font-size-xxs: 0.2em;
+}
+
+/* font-weight */
+:root {
+    --font-weight-xxl: ;
+    --font-weight-xl: ;
+    --font-weight-l: ;
+    --font-weight-m: ;
+    --font-weight-s: ;
+    --font-weight-xs: ;
+    --font-weight-xxs: ;
+}
+
+/* colors? */
+:root {
+    --color-1: black;
+    --color-2: white;
+    --color-3: beige;
+    --color-4: ;
+}
+</style>
