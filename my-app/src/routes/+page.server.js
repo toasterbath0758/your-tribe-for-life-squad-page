f647181ccedcf6b6b39137330de7ac9829d7b988
bronxpostma13 <bronxpostma@gmail.com>
2026-09-02 15:21:18 +0200

    server js push

---
+++ my-app/src/routes/+page.server.js
@@ -0,0 +1,7 @@
+export async function load(){
+const res = await fetch ("https://fdnd.directus.app/items/person?filter[squads][squad_id][cohort][_eq]=2627")
+const data = await res.json();
+
+return {persons: data.data}
+       
+}
