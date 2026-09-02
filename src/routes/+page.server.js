7f944021fe40986cff3c5007689549b05b569c25
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-02 16:55:50 +0200

    server.js setup #2

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,9 @@
 export async function load(){
-const res = await fetch ("https://fdnd.directus.app/items/person?filter[squads][squad_id][cohort][_eq]=2627")
-const data = await res.json();
- 
-return {persons: data. Data}
-       
+    const res = await fetch ("https://fdnd.directus.app/items/person?fields=name,fav_color,fav_emoji,vibe_emoji&filter[squads][squad_id][cohort][_eq]=2627")
+    const dataPersons = await res.json()
+    
+    return {persons: dataPersons.data}
 }
+
+
+
