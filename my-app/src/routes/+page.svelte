9f7abf8e3c64f7e67b6ec1727de6d3fe5a4e35bb
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-02 15:01:04 +0200

    +page.svelte setup #2

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+Let {data} = $props();
+Const persons = data.persons;
+</script>
+
+{#each persons as person}
+{person.name}
+{/each}
+
+ <h1> <h1>
+<p> hallo mensen </p>
