0478d7ee60e0678e59f29b62fa8a65873edc2fe9
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-02 15:46:50 +0200

    setup svelte #2

--- my-app/src/routes/+page.server.js
+++
@@ -1,7 +0,0 @@
-export async function load(){
-const res = await fetch ("https://fdnd.directus.app/items/person?filter[squads][squad_id][cohort][_eq]=2627")
-const data = await res.json();
-
-return {persons: data.data}
-       
-}
