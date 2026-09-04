683c6cf49ae4200eff61961eaeb1bceb927963b8
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-04 15:51:55 +0200

    added alle routes van data voor rendering #2

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 export async function load(){
-    const res = await fetch ("https://fdnd.directus.app/items/person?fields=name,fav_color,fav_emoji,vibe_emoji&filter[squads][squad_id][cohort][_eq]=2627")
+    const res = await fetch ("https://fdnd.directus.app/items/person?fields=id,name,mugshot,fav_color,fav_emoji,vibe_emoji&filter[squads][squad_id][cohort][_eq]=2627")
     const dataPersons = await res.json()
     
     return {persons: dataPersons.data}
