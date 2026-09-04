28aca51e43e6a34e5758d1dae014762c1c34fa08
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-04 15:59:22 +0200

    fixed data loading op de homepage #4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -115,16 +115,13 @@
 </style>
 
 <script>
-    const {data} = $props()
-    const persons = data.persons
 
-    console.log(persons);
-    
+	import { onMount } from 'svelte';
+
+	const { data } = $props();
+	const persons = data.persons;
+
+
+
 </script>
 
-{#each persons as person }
-    {person.name} <br>
-    {person.fav_emoji} <br>
-    {person.vibe_emoji} <br>
-    
-{/each}
