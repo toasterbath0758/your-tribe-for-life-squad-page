58910ba27bbca9193c3b6c170f86ea533653c652
toasterbath0758 <toasterbath0758@gmail.com>
2026-09-08 15:09:32 +0200

    layout page header fix #13

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,36 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<header>
+    <h1>jan jaap en de rest</h1>
+</header>
+
+
+
 {@render children()}
+
+<style>
+
+	* {
+		margin: 0;
+	    padding: 0;
+	    font-family: cooper;
+	    color: darkslategrey;
+	}
+
+
+
+    header {
+		padding: 1em;
+        display: flex;
+        z-index: 10;
+		width: 100vw;
+
+        background: green;
+
+		h1 {
+			color: lightgreen;
+		}
+    }
+
+</style>
