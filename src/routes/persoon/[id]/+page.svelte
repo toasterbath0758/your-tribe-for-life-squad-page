f26f7daf6d965495c2bcd4887085d6df4acd16ac
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-09-08 12:03:13 +0200

    #5 styling detial pagina

--- src/routes/persoon/[id]/+page.svelte
+++ src/routes/persoon/[id]/+page.svelte
@@ -54,15 +54,78 @@
 
 	<p>Persoon niet gevonden.</p>
 
-		border-radius: 8px;
-	}
+{/if}
 
-	/* Naam */
-	.person-card h1 {
-		margin-top: 2rem;
+<style>
 
-		font-size: 2.5rem;
-		font-weight: 400;
+body{
+
+	margin: 0;
+}
+	.detail-page {
+		min-height: 100vh;
+        display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 4rem 1rem;
         background-image: url('/images/pinboard.jpg');
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+
+		.polaroid {
+			position: relative;
+            width: min(80vw, 340px);
+            background-color: white;
+            padding: 1rem 1rem 2rem;
+
+			.tape {
+				position: absolute;
+                width: 90px;
+				top: -35px;
+				left: 40%;
+                z-index: 2;
+			}
+
+			.person-photo {
+				width: 100%;
+				aspect-ratio: 1 / 1;
+                display: block;
+                object-fit: cover;
+                background-color: black;
+			}
+
+			.person-info {
+				margin-top: 1rem;
+
+				h1 {
+					margin: 0 0 1rem;
+                    font-size: 1rem;
+					font-weight: 400;
+				}
+
+				 p {
+					margin: 0 0 1rem;
+					line-height: 1.5;
+				}
+
+				.favorite-color {
+					display: flex;
+					align-items: center;
+					gap: 1rem;
+
+					p {
+						margin: 0;
+					}
+
+					.color-block {
+						display: inline-block;
+
+						width: 22px;
+						height: 22px;
+					}
+				}
+			}
+		}
 	}
 </style>
